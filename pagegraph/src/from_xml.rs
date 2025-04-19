8358f78410849e77127725ceabfd0ed809d36e1b
Arxhexa <98107500+Arxhexa@users.noreply.github.com>
2025-04-18 21:27:24 -0500

    Update from_xml.rs

--- pagegraph/src/from_xml.rs
+++ pagegraph/src/from_xml.rs
@@ -97,12 +97,8 @@ fn build_desc<R: std::io::Read>(
     let version = version.ok_or_else(|| "Version not found in desc").unwrap();
     let about = about.ok_or_else(|| "About not found in desc").unwrap();
     let url = url.ok_or_else(|| "Url not found in desc").unwrap();
-    let is_root = is_root.ok_or_else(|| "Is_root not found in desc")
-        .and_then(|s| s.parse::<bool>().map_err(|e| format!("Failed to parse is_root: {}", e)))
-        .unwrap();
-    let frame_id = frame_id.ok_or_else(|| "Frame_id not found in desc")
-        .and_then(|s| graph::FrameId::try_from(s.as_str()).map_err(|e| format!("Failed to parse frame_id: {}", e)))
-        .unwrap();
+    let is_root = is_root.ok_or_else(|| "Is_root not found in desc").and_then(|s| s.parse::<bool>().map_err(|e| format!("Failed to parse is_root: {}", e))).unwrap();
+    let frame_id = frame_id.ok_or_else(|| "Frame_id not found in desc").and_then(|s| graph::FrameId::try_from(s.as_str()).map_err(|e| format!("Failed to parse frame_id: {}", e))).unwrap();
     let time = time.ok_or_else(|| "Time not found in desc").unwrap();
 
     graph::PageGraphDescriptor {
